b589b09a8f4b54f997de92b42af843ae3eaa05f3
asikaer nadire <nadi9594@gmail.com>
2022-09-23 23:25:20 -0500

    day4 HomeWork XpathPractice Done

--- src/test/java/com/cydeo/tests/homeWork/Day4Homework/XpathPractice.java
+++ src/test/java/com/cydeo/tests/homeWork/Day4Homework/XpathPractice.java
@@ -1,12 +1,34 @@
 package com.cydeo.tests.homeWork.Day4Homework;
 
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.concurrent.TimeUnit;
+
 public class XpathPractice {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
+        driver.get("http://practice.cydeo.com/multiple_buttons ");
 
+        WebElement button1 = driver.findElement(By.xpath("//button[@onclick='button1()']"));
+        button1.click();
+        WebElement clickText = driver.findElement(By.xpath("//p[@id='result']"));
 
+        String actual = clickText.getText();
+        String expected = "Clicked on button one!";
+        if(actual.equals(expected)){
+            System.out.println("Passed");
+        }else{
+            System.out.println("Failed");
+        }
 
-
+        Thread.sleep(3000);
+        driver.quit();
 
     }
 }
