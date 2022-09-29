188178c8f42f52ce4fc07ad4811147c77e56ee5d
asikaer nadire <nadi9594@gmail.com>
2022-09-29 11:34:44 -0500

    T2 created under Day5

---
+++ src/test/java/com/cydeo/tests/day5_testNG_dropDown/T2_StaleElementRefrenceException.java
@@ -0,0 +1,58 @@
+package com.cydeo.tests.day5_testNG_dropDown;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class T2_StaleElementRefrenceException {
+    public static void main(String[] args) {
+        // 1. Open Chrome browser
+        WebDriver driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        // 2. Go to https://practice.cydeo.com/add_remove_elements/
+        driver.get("https://practice.cydeo.com/add_remove_elements/");
+        //  3. Click to “Add Element” button
+
+
+     //   4. Verify “Delete” button is displayed after clicking.
+
+     //   5. Click to “Delete” button.
+
+    //    6. Verify “Delete” button is NOT displayed after clicking. US
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
