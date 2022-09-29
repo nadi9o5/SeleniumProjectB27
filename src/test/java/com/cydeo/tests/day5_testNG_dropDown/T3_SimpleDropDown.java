c46d75b153b04b29ab61970865ecd5f11c3bde7f
asikaer nadire <nadi9594@gmail.com>
2022-09-29 15:31:30 -0500

    T3 created under Day5

---
+++ src/test/java/com/cydeo/tests/day5_testNG_dropDown/T3_SimpleDropDown.java
@@ -0,0 +1,35 @@
+package com.cydeo.tests.day5_testNG_dropDown;
+
+public class T3_SimpleDropDown {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- src/test/java/com/cydeo/tests/day5_testNG_dropDown/TestNG_Selenium.java
+++ src/test/java/com/cydeo/tests/day5_testNG_dropDown/TestNG_Selenium.java
@@ -3,26 +3,39 @@ package com.cydeo.tests.day5_testNG_dropDown;
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 public class TestNG_Selenium {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod(){
+        driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
 
     @Test
     public void googleTitle(){
-        WebDriver driver= WebDriverFactory.getDriver("chrome");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         driver.get("https://www.google.com/");
 
             String actualTitle=driver.getTitle();
             String expectedTitle="Google";
 
-        Assert.assertEquals(actualTitle,expectedTitle);// if statement
+        Assert.assertEquals(actualTitle,expectedTitle,"Title is not matching");// if statement
     }
 
+    @AfterMethod
+    public void  tearDownMethod() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.quit();
+    }
 
 
 
